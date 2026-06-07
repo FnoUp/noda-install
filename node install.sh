0276ebf93e7e470aef2153a421a0acaad7eece60
FnoUp <4nosveraty@gmail.com>
2026-06-07 17:41:11 +0500

    node install.sh

--- node install.sh
+++ node install.sh
@@ -10,18 +10,13 @@ export DEBIAN_FRONTEND=noninteractive
 
 apt update
 apt upgrade -y
+apt autoremove -y
 
-apt install -y 
-curl 
-wget 
-git 
-socat 
-net-tools 
-ufw
+apt install -y curl wget git socat net-tools ufw
 
 echo "Installing Remnawave Reverse Proxy..."
 
-bash <(curl -Ls https://raw.githubusercontent.com/eGamesAPI/remnawave-reverse-proxy/refs/heads/main/install_remnawave.sh)
+bash <(curl -Ls "https://raw.githubusercontent.com/eGamesAPI/remnawave-reverse-proxy/refs/heads/main/install_remnawave.sh")
 
 echo "=================================="
 echo "Installation completed"
